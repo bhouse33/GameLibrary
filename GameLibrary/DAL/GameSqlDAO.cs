8b6a4eb9a796295dec53b606c00e9ee9e7d4b484
Matthew Dunavant <madunavant@gmail.com>
2019-03-11 14:49:27 -0400

    Created GameModel and GetGames() method

--- GameLibrary/DAL/GameSqlDAO.cs
+++ GameLibrary/DAL/GameSqlDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GameLibrary.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,57 @@ namespace GameLibrary.DAL
 {
     public class GameSqlDAO
     {
-        // dont do this
+        private string ConnectionString { get; }
 
+        public GameSqlDAO(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public IList<GameModel> GetGames()
+        {
+            IList<GameModel> games = new List<GameModel>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.ConnectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("select * from games", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        GameModel game = ConvertReaderToGameModel(reader);
+                        games.Add(game);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return games;
+        }
+
+        private GameModel ConvertReaderToGameModel(SqlDataReader reader)
+        {
+            GameModel game = new GameModel()
+            {
+                Title = Convert.ToString(reader["title"]),
+                ImageUrl = Convert.ToString(reader["image"]),
+                Description = Convert.ToString(reader["description"]),
+                MinPlayers = Convert.ToInt32(reader["min_players"]),
+                MaxPlayers = Convert.ToInt32(reader["max_players"]),
+                ReccommendedAge = Convert.ToInt32(reader["recommended_age"]),
+                AvgPlayTime = Convert.ToInt32(reader["avg_play_time"]),
+                BGGRating = Convert.ToDouble(reader["bgg_rating"]),
+                BGGWeight = Convert.ToDouble(reader["bgg_weight"])
+            };
+
+            return game;
+        }
     }
 }
--- GameLibrary/Models/GameModel.cs
+++ GameLibrary/Models/GameModel.cs
@@ -7,5 +7,18 @@ namespace GameLibrary.Models
 {
     public class GameModel
     {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public int ReccommendedAge { get; set; }
+        public int AvgPlayTime { get; set; }
+        public double BGGWeight { get; set; }
+        public double BGGRating { get; set; }
+        public bool IsBorrowed { get; set; }
+        public string Genre { get; set; }
+
+        public IList<string> Genres { get; set; }
     }
 }
